06139e4916e1280ccd674762fb5380782e5bc6ae
Vishnu Reddy <134081551+Vishnutime1@users.noreply.github.com>
2025-11-10 15:01:28 +0000

    Basic functions of operators in Javascript

---
+++ 01_JS Foundation/02.Operators.js
@@ -0,0 +1,64 @@
+let score = 100
+let bonus = 25
+
+let total_pay = score + bonus
+let deduct_tax = score - bonus
+let job_switch = score * bonus
+let divi = score / bonus
+let remai = score / bonus
+let power = score ** 2
+
+
+// console.log(total_pay)
+// console.log(deduct_tax);
+// console.log(job_switch);
+// console.log(divi);
+// console.log(remai);
+// console.log(power);
+
+/* Comparison operators */
+
+num1 = 5
+num2 = 10
+num3 = 5
+
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num3)
+
+// Logical operators
+
+// && => Logical AND
+// OR => logical OR
+
+let isStudent = true
+let asID = true
+let eventPass = false
+
+// console.log("Enter college", isStudent && asID);
+// console.log("Don't enter final event", isStudent && eventPass);
+// console.log("Enter sample event" , isStudent || eventPass);
+
+// ?Assignment operators
+
+numb1 = 4
+numb2 = 5
+
+numb1 += numb1
+numb1 += numb2
+
+// console.log(numb1);
+// console.log(numb2);
+
+
+
+
+
+
+
+
+
+
+
