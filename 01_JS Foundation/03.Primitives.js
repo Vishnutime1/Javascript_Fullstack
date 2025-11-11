d7099d43e713bdcd4cbd80fb8e2e9905f374cba8
Vishnu Reddy <134081551+Vishnutime1@users.noreply.github.com>
2025-11-11 16:05:58 +0000

    Discussed about main functionalities of Primitive and Non primitive Datatypes in JS

---
+++ 01_JS Foundation/03.Primitives.js
@@ -0,0 +1,50 @@
+// Primitive  Datatype
+// 1.Strings
+// 2. Number
+// 3.Null
+// 4.Boolean
+// 5.Undefined
+// 6. Symbol
+
+// Numbers
+
+let num1 = 10
+let score = new Number(1950)
+
+console.log(num1);
+console.log(score);
+
+console.log(typeof num1);
+console.log(typeof score);
+
+// ?? Primitived Can be coverted to Non premitives
+
+// ??Strings
+
+let f_name = "Vishnu"
+let m_name = 'vardhan'
+let l_name = `Reddy`
+
+let Greeting = `Hi ${f_name}`
+let appre = f_name +" Good"
+console.log(Greeting);
+console.log(appre);
+
+// null undefined - To specify uniquness
+
+let chill;
+let full = null;
+
+console.log(chill);
+console.log(full);
+
+// Symbol
+
+let Hi = Symbol()
+let Hello = Symbol
+
+console.log(Hi == Hello);
+
+
+
+
---
+++ 01_JS Foundation/04_Non-Primitive.js
@@ -0,0 +1,31 @@
+//Object, Array, Functions
+
+let details = {
+    f_name : "Vishnu",
+    logged_in :true,
+    "is Check" : "Checked"
+}
+console.log(typeof details);
+console.log(details);
+
+console.log(details.f_name);
+details.f_name = "Madduri"
+console.log(details);
+
+details.l_name = "Reddy"
+
+console.log(details);
+
+console.log(details["is Check"]);
+
+//Array
+
+let this_Array = ["Hi", "there", 1, 2, ["Yo","yep"]]
+
+console.log(this_Array[0]);
+console.log(this_Array);
+console.log(this_Array[4][1]);
+
+
+
+
